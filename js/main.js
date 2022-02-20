55b4674e31d40de1d52b89800fd5dacb13eac67b
jerryshen26 <fast199111@yahoo.com.tw>
2022-02-20 22:51:21 +0800

    修改：網站的版面調整與樣式

--- js/main.js
+++ js/main.js
@@ -1,32 +1,120 @@
-window.onload = function () {
+window.onload = function() {
+
+    var checkClick = true;
+    var sectionIds = {};
+
+
     initTyping();
+
     function initTyping() {
         let lineIndex = 0;
         let isBacking = false;
         setInterval(() => {
-          let textArr = ["Put into practice. ", "Self-development. ", "Positive. "];
-          let element = document.querySelector(".typingText");
-          let vvv = element.textContent;
-          let index = vvv.length;
-          let fullText = textArr[lineIndex];
-          if (isBacking) {
-            index--;
-          } else {
-            index++;
-          }
-          if (index === fullText.length) {
-            isBacking = !isBacking;
-          }
-          if (index === -1) {
-            isBacking = !isBacking;
-            if (lineIndex < textArr.length - 1) {
-              lineIndex++;
+            let textArr = ["UI & Website Design.", "Develop Website", "Positive. "];
+            let element = document.querySelector(".typingText");
+            let vvv = element.textContent;
+            let index = vvv.length;
+            let fullText = textArr[lineIndex];
+            if (isBacking) {
+                index--;
             } else {
-              lineIndex = 0;
+                index++;
             }
-          }
-          let newText = fullText.substring(0, index);
-          element.textContent = newText;
+            if (index === fullText.length) {
+                isBacking = !isBacking;
+            }
+            if (index === -1) {
+                isBacking = !isBacking;
+                if (lineIndex < textArr.length - 1) {
+                    lineIndex++;
+                } else {
+                    lineIndex = 0;
+                }
+            }
+            let newText = fullText.substring(0, index);
+            element.textContent = newText;
         }, 200);
-      }
+    }
+
+
+    $(".section").each(function() {
+        var $this = $(this);
+        sectionIds[$this.attr("id")] = Math.ceil($this.first().offset().top) - 5;
+    });
+
+    $(window).scroll(function(evet) {
+        if (checkClick) {
+            var scrolled = $(this).scrollTop();
+            var windowHeight = $(window).innerHeight();
+            var bodyHeight = $("body").height();
+            for (key in sectionIds) {
+                if ($(this).scrollTop() == (bodyHeight - windowHeight)) {
+                    $(".portfolio-menu li").removeClass("active");
+                    var c = $("[data-id=" + key + "]");
+                    c.addClass("active");
+                } else if (scrolled >= sectionIds[key]) {
+                    $(".portfolio-menu li").removeClass("active");
+                    var c = $("[data-id=" + key + "]");
+                    c.addClass("active");
+                }
+            }
+        }
+    });
+
+    $(".portfolio-menu li").click(function() {
+        $(this).addClass('active');
+        $(this).siblings().removeClass('active')
+
+        checkClick = false;
+        var name = $(this).attr('data-id');
+        var id = "#" + name;
+        var top = $(id).first().offset().top;
+        $('html, body').animate({ scrollTop: top + 'px' }, 100, null, function() {
+            checkClick = true;
+        });
+    });
+
+    fabClickFun();
+
+    $(".portfolio-prototype-menu li").click(function() {
+        $(".portfolio-prototype-menu li").removeClass("active");
+        $(this).addClass("active");
+    })
+
+}
+
+function fabClickFun() {
+    var winWidth = $(window).width();
+    // $(".edm-nav-toggle,.fabb").unbind();
+    if (winWidth <= 1000) {
+        $(".portfolio-nav-menu,.fabb").click(function() {
+            if ($(".portfolio-menu").is(":visible")) {
+                $(".portfolio-menu").fadeOut(100);
+                $(".portfolio-nav-toggle").removeClass("edm-nav-toggle-cancel");
+                $(".portfolio-content").removeClass("content-blur");
+            } else {
+                $(".portfolio-menu").fadeIn(100);
+                $(".portfolio-nav-toggle").addClass("edm-nav-toggle-cancel");
+                $(".portfolio-content").addClass("content-blur");
+            }
+        });
+    }
+}
+
+
+
+function changePrototypeData(type) {
+    var $prototypeCard = $(".portfolio-prototype-block .portfolio-prototype-card");
+
+    $prototypeCard.fadeIn(0);
+    $(".portfolio-menu li")[2].click();
+    if (type == "all")
+        return
+    $prototypeCard.each(function() {
+        var data = $(this).data("item");
+        if (data != type) {
+            $(this).fadeOut(0);
+        }
+    });
+
 }
