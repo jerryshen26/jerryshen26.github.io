43673d22db3fa45a4512acacda7ca77adba9a020
jerryshen26 <fast199111@yahoo.com.tw>
2024-01-21 12:30:08 +0800

    變更sample與new year的資訊與版面調整

--- js/uiViewPage.js
+++ js/uiViewPage.js
@@ -1,12 +1,15 @@
 window.onload = function() {
+    const openHeight = "90vh"
+    $(".ui-design-prototype").css({"height":openHeight});
     $(".ui-design-prototype-learnmore p").click(function() {
         if ($(".ui-design-prototype-learnmore p.active").length == 0) {
             $(".ui-design-prototype").css({ "height": "auto", "overflow": "auto" });
             $(".ui-design-prototype-learnmore p").addClass("active");
 
         } else {
-            $(".ui-design-prototype").css({ "height": "100vh", "overflow": "hidden" });
+            $(".ui-design-prototype").css({ "height": "90vh", "overflow": "hidden" });
             $(".ui-design-prototype-learnmore p").removeClass("active");
+            $(window).scrollTop($(".ui-design-prototype").offset().top);
         }
     })
 
