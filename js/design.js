2d0c51ac23ca01484129d7f602b94c781557b08c
jerryshen26 <fast199111@yahoo.com.tw>
2024-01-21 14:24:11 +0800

    再次修正design頁面的排版與內容的更新，與更新專案內容

--- js/design.js
+++ js/design.js
@@ -1,26 +1,37 @@
 function changePrototypeData(type) {
     var $prototypeCard = $(".portfolio-prototype-block .portfolio-prototype-card");
 
+    $(".portfolio-prototype-card").css({
+        width: "0%"
+    });
+    var changeWidth = function(){
+        $(".portfolio-prototype-card").animate({ width: "22%" },20);
+    }
+
     $prototypeCard.fadeIn(0);
-    $(".portfolio-menu li")[1].click();
-    if (type == "all")
+    $(".portfolio-menu li")[0].click();
+    if (type == "all"){
+    changeWidth();
         return
-    $prototypeCard.each(function() {
+    }
+    $prototypeCard.each(function () {
         var data = $(this).data("item");
         var boolCheck = checkData(data, type);
+
         if (!boolCheck) {
             $(this).fadeOut(0);
         }
     });
+    changeWidth();
 
 }
+
 
 function checkData(data, type) {
     var datasplit = data.split(",");
     var checkContent = false;
-    datasplit.forEach(function(d) {
+    datasplit.forEach(function (d) {
         if (d == type) {
-            debugger
             checkContent = true;
         }
     });
